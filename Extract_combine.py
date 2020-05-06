3deff18de9d5e540b91e7937903db125b43b7a85
kambojrakesh <rakeshkamboj1826@gmail.com>
2020-05-06 15:49:08 +0530

    Extract combine commit

--- Extract_combine.py
+++ Extract_combine.py
@@ -4,9 +4,12 @@ Created on Mon May  4 22:23:10 2020
 
 @author: Vikki
 """
+import requests
+import sys
 import pandas as pd
 from bs4 import BeautifulSoup
-
+import os
+import csv
 from Plot_AQI import avg_data_2013,avg_data_2014,avg_data_2015,avg_data_2016,avg_data_2017,avg_data_2018
 
 def met_data(month, year):
@@ -32,10 +35,87 @@ def met_data(month, year):
             tempD.pop(0)
             
         finalD.append(newtempD)
+        
+    length = len(finalD)     
             
-               
+    finalD.pop(length - 1)
+    finalD.pop(0)                 
                  
-   ## print(soup)
+    for a in range(len(finalD)):
+        finalD[a].pop(6)
+        finalD[a].pop(13)
+        finalD[a].pop(12)
+        finalD[a].pop(11)
+        finalD[a].pop(10)
+        finalD[a].pop(9)
+        finalD[a].pop(0)
+    
+    return finalD
+    
+def data_combine(year, cs):
+    for a in pd.read_csv('Data/Real-Data/real_' + str(year) + '.csv', chunksize=cs):
+        df = pd.DataFrame(data=a)
+        mylist = df.values.tolist()
+    return mylist
+
 
 if __name__ == "__main__":
-    met_data(1,2013)
+    if not os.path.exists("data/Real-Data"):
+        os.makedirs("data/Real-Data")
+        
+    for year in range(2013 , 2017):
+        final_data = []
+        with open('data/Real-Data/real_'+str(year)+'.csv', 'w') as csvfile:
+            wr = csv.writer(csvfile, dialect='excel')
+            wr.writerow(
+                ['T', 'TM', 'Tm', 'SLP', 'H', 'VV', 'V', 'VM', 'PM 2.5'])
+        for month in range(1,13):
+            temp = met_data(month, year)
+            final_data = final_data + temp
+            
+    ##print(getattr(sys.modules[__name__], 'avg_data_{}'.format(year))())
+        pm = getattr(sys.modules[__name__], 'avg_data_{}'.format(year))()
+        
+        for i in range(len(final_data)-1):
+            final_data[i].insert(8, pm[i])
+            
+        with open('Data/Real-Data/real_' + str(year) + '.csv', 'a') as csvfile:
+            wr = csv.writer(csvfile, dialect='excel')
+            for row in final_data:
+                flag = 0
+                for elem in row:
+                    if elem == "" or elem == "-":
+                        flag = 1
+                if flag != 1:
+                    wr.writerow(row)
+    
+    data_2013 = data_combine(2013, 600)
+    data_2014 = data_combine(2014, 600)
+    data_2015 = data_combine(2015, 600)
+    data_2016 = data_combine(2016, 600)
+     
+    total=data_2013+data_2014+data_2015+data_2016
+    
+    with open('Data/Real-Data/Real_Combine.csv', 'w') as csvfile:
+        wr = csv.writer(csvfile, dialect='excel')
+        wr.writerow(
+            ['T', 'TM', 'Tm', 'SLP', 'H', 'VV', 'V', 'VM', 'PM 2.5'])
+        wr.writerows(total)
+        
+        
+df=pd.read_csv('Data/Real-Data/Real_Combine.csv')    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
